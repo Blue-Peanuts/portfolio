16602f063330e1e510b27d3e53da6a76a0da8927
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-02-04 22:50:05 -0500

    Tab created

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,66 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Button, createTheme, ThemeProvider, Drawer } from '@mui/material'
+import { Button, createTheme, ThemeProvider, Drawer, Grid, Box } from '@mui/material'
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports'
+import AudiotrackIcon from '@mui/icons-material/Audiotrack'
+import PaletteIcon from '@mui/icons-material/Palette'
+
 const theme = createTheme({
     palette: {
-        secondary: {
+        primary: {
             main: '#ffffff'
+        },
+        background: {
+            main: "#131e2a"
         }
     }
 });
+
 function App() {
     const [count, setCount] = useState(0)
-
+    const [open, setOpen] = useState(true);
+    const toggleSlider = () => {
+        setOpen(!open);
+    };
+    const iconPretextStyle = {
+        margin: '0 4px 0 0',
+    }
+    //create a style const to align the object to the left
+    const contentAlignLeft = { justifyContent: 'flex-start' }
     return (
-        <ThemeProvider theme={theme}>
-            <div className="App">
-                <Button variant="outlined" color="secondary">
-                    I am a button Ia get turned on when u click.
-                </Button>
-                
 
+        <ThemeProvider theme={theme}>
+
+            <div className="roboto">
+                <Drawer open={open} anchor="left" onClose={toggleSlider}>
+                    <Box sx={{
+                        backgroundColor: 'background.main',
+                        width: 200,
+                        height: '100vh'
+                    }}>
+                        <Grid
+                            container
+                            direction="column"
+                            justifyContent="text"
+                            alignItems="stretch"
+                        >
+                            <Button variant="text" color="primary" style={{ justifyContent: "flex-start" }}>
+                                <SportsEsportsIcon style={iconPretextStyle} />
+                                <span>Games</span>
+                            </Button>
+                            <Button variant="text" color="primary">
+                                <AudiotrackIcon style={iconPretextStyle} />
+                                <span>Music</span>
+                            </Button>
+                            <Button variant="text" color="primary">
+                                <PaletteIcon style={iconPretextStyle} />
+                                <span>Drawings</span>
+                            </Button>
+
+                        </Grid>
+                    </Box>
+                </Drawer>
             </div>
         </ThemeProvider>
     )
