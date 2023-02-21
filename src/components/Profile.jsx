ecdcfde4e7f0619c699d72f46bf75c6e07400345
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-02-21 11:53:10 -0500

    clean up stuff to prepare for style change

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,92 +2,47 @@ import React from 'react';
 import { Box, Fade, Slide, Grow, Avatar } from '@mui/material';
 
 const Profile = () => {
-  return (
-    <Box sx={{
-        marginTop: '50px',
-        display: 'flex',
-        flexdirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-    }}>
-      <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'left',
-          marginTop: '-30px',
-      }}>
-        <Fade timeout={2000} in={true}>
-          <Box sx={{
-              fontSize: '120px',
-              textAlign: 'left',
-          }}>NITANON</Box>
-        </Fade>
-        <Fade in={true} timeout={600}>
-          <div>
-            <Slide direction='right' in={true} timeout={600}>
-              <Box sx={{
-                  width: '105%',
-                  marginLeft: '-20px',
-                  backgroundColor: 'primary.main',
-                  height: '4px',
-                  marginTop: '-30px',
-              }} />
-            </Slide>
-          </div>
-        </Fade>
-        <Fade timeout={1500} in={true}>
-          <Box sx={{
-              fontSize: '50px',
-              textAlign: 'left',
-              marginTop: '-30px',
-          }}>RACHAPRADIT</Box>
-        </Fade>
-      </Box>
-      <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginLeft: '60px',
-          marginTop: '30px',
-      }}>
-        <Fade in={true} timeout={2000}>
-          <Box sx={{
-              backgroundColor: 'primary.main',
-              borderRadius: '50%',
-              border: '4px solid',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-          }}>
-            <Grow in={true} timeout={1200}>
-              <Avatar sx={{
-                  width: '200px',
-                  height: '200px',
-                  border: '20px solid',
-                  borderColor: 'background.main',
-              }}
-                alt="Nitanon Rachapradit" src="./src/assets/profile.jpg" />
-            </Grow>
-          </Box>
-        </Fade>
+    return (
         <Box sx={{
-            textAlign: 'center',
-            marginTop: '20px',
-            fontSize: '16px',
-            color: 'primary.main',
+            marginTop: '50px',
+            display: 'flex',
+            flexdirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
         }}>
-          <Fade in={true} timeout={2000}>
-            <div>
-              Also known as "Blue Peanuts"
-            </div>
-          </Fade>
+            <Box>
+                <Box sx={{
+                    fontSize: '11vw',
+                    textAlign: 'left',
+                    backgroundColor: 'primary.main',
+                    color: 'background.main',
+                    display: 'flex',
+                    justifyContent: 'left',
+                    verticalAlign: 'middle',
+                    alignItems: 'center',
+                    width: '70vw',
+                    height: '12vw',
+                    paddingLeft: '1vw',
+                }}>NITANON</Box>
+                <Box sx={{
+                    fontSize: '7vw',
+                    textAlign: 'left',
+                    color: 'primary.main',
+                    display: 'flex',
+                    justifyContent: 'left',
+                    verticalAlign: 'middle',
+                    alignItems: 'center',
+                }}>RACHAPRADIT</Box>
+            </Box>
+            <Box sx={{
+                width: '30vw',
+                height: '30vw',
+                backgroundColor: 'primary.main',
+                backgroundImage: './src/assets/profile.jpg',
+            }}></Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default Profile;
