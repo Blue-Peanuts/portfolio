b98811f5dd8421914acdec5adab919758450af54
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-09-09 23:54:01 +0700

    profileFinish

--- src/InfoCard/ProfilePicture.tsx
+++ src/InfoCard/ProfilePicture.tsx
@@ -1,8 +1,13 @@
 import profilePicture from '../assets/profile.jpg';
-function ProfilePicture()
-{
+function ProfilePicture() {
     return <div>
-        <img src={profilePicture} className=" w-28 h-28 object-cover rounded-full" alt=""/>
+        <div className=' w-fit flex justify-center items-center border-4 rounded-lg border-sky-500 p-3 rotate-12'>
+            <div className=' w-fit flex justify-center items-center border-4 rounded-lg border-orange-500 p-1 rotate-12'>
+                <div className = '-rotate-12'>
+                    <img src={profilePicture} className=" m-1 w-28 h-28 object-cover rounded-full -rotate-12" alt="" />
+                </div>
+            </div>
+        </div>
     </div>
 }
 
