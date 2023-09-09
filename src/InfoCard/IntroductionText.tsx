b70d582e630f73fc5c1ec0ee780c9f03f6fd415c
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-09-09 23:08:19 +0700

    / to the end

--- src/InfoCard/IntroductionText.tsx
+++ src/InfoCard/IntroductionText.tsx
@@ -14,7 +14,7 @@ function IntroductionText() {
                     </div>
                 </div>
                 <div className=' flex flex-row '>
-                    <div className=' w-48 h-1 bg-white mt-2 mr-2'></div>
+                    <div className=' w-48 h-1 bg-white mt-2 mr-2'/>
                     <div className=' text-sm font-bold'>
                         BLUE PEANUTS
                     </div>
