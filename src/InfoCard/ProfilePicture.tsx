2eeb17ed87dff0e62d240c10e807259543398886
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-09-10 17:38:46 +0700

    prof respo fix

--- src/InfoCard/ProfilePicture.tsx
+++ src/InfoCard/ProfilePicture.tsx
@@ -3,13 +3,13 @@ function ProfilePicture() {
     return <div>
         <div className=' 
         w-56 rounded-2xl border-4
-        md:w-80 sm:rounded-3xl sm:border-8 
+        md:w-80 md:rounded-3xl md:border-8 
         lg:w-96 lg:rounded-3xl lg:border-8
         aspect-square flex justify-center items-center border-sky-500 rotate-12
         '>
             <div className=' 
             w-10/12 border-4 rounded-2xl
-            sm:w-10/12 sm:border-8 sm:rounded-3xl
+            md:w-10/12 md:border-8 md:rounded-3xl
             aspect-square flex justify-center items-center border-orange-500 rotate-12'>
                 <div className = '-rotate-12 w-full h-full flex justify-center items-center'>
                     <img src={profilePicture} className=" w-11/12 aspect-square object-cover rounded-full -rotate-12" alt="" />
