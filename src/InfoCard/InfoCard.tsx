f0ad32fc42e4e1bf6f812e4d1193e60cc68b24aa
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-09-10 19:23:10 +0700

    fullscreen infocard finished

--- src/InfoCard/InfoCard.tsx
+++ src/InfoCard/InfoCard.tsx
@@ -1,3 +1,4 @@
+import BluePicture from "./BluePicture";
 import IntroductionText from "./IntroductionText";
 import ProfilePicture from "./ProfilePicture";
 import WorkList from "./WorkList";
@@ -8,15 +9,23 @@ function InfoCard() {
             <div className=' w-fit flex
             flex-col gap-12 justify-between 
             items-center
-            lg:flex-row lg:gap-32 lg:items-end pt-20
+            xl:flex-row xl:gap-32 xl:items-end pt-20
+            
             '>
                 <div className='w-fit pb-12'>
                     <ProfilePicture />
                 </div>
                 <div className=' flex flex-col justify-start items-start gap-8'>
                     <IntroductionText />
-                    <WorkList />
+                    <div className='flex flex-row items-end gap-16'>
+                        <WorkList />
+                        <div className='w-fit h-full -mb-7'>
+                            <BluePicture />
+                        </div>
+                    </div>
                 </div>
+
+
             </div>
         </div>
     );
--- src/InfoCard/WorkList.tsx
+++ src/InfoCard/WorkList.tsx
@@ -3,7 +3,7 @@ function WorkList() {
         <div className=' text-xs sm:text-lg mb-2'>
             I make...
         </div>
-        <div className=' pt-4 flex flex-col gap-4'>
+        <div className=' pt-4 flex flex-col gap-3'>
             <WorkButton text='Video Games' className='bg-red-500 ' />
             <WorkButton text='Apps and Tools' className='bg-green-500 ' />
             <WorkButton text='Music' className='bg-cyan-500 ' />
@@ -15,8 +15,8 @@ function WorkList() {
 function WorkButton(props: { text: string, className: string }) {
 
     return <button className={` ${props.className} font-bold text-sm w-fit rounded-tl-full rounded-br-full`}>
-        <div className=' ml-5 mr-28
-                                pt-1 pb-1.5
+        <div className=' ml-5 mr-36
+                                pt-0.5 pb-1
                         text-sm 
                         sm:text-3xl'>
             &bull;
