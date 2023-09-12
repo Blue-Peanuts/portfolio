d31c135bc13a072ff3f523ddccb54642480360e5
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-09-12 23:04:09 +0700

    buttons rework

--- src/InfoCard/WorkList.tsx
+++ src/InfoCard/WorkList.tsx
@@ -1,7 +1,19 @@
 import ScrollAnimation from "../Animations/ScrollAnimation";
 import '../Animations/Animations.scss'
 
+import { useState } from "react";
+
 function WorkList() {
+    const [hoverCount, setHoverCount] = useState(0);
+
+    function hoverIncrementCallback() {
+        setHoverCount(prevHoverCount => prevHoverCount + 1);
+    }
+
+    function hoverDecrementCallback() {
+        setHoverCount(prevHoverCount => prevHoverCount - 1);
+    }
+
     return <div>
         <ScrollAnimation initialClass='opacity-none' animationClass='blur-reveal-4'>
             <div className=' text-xs sm:text-lg mb-2'>
@@ -9,27 +21,45 @@ function WorkList() {
             </div>
         </ScrollAnimation>
         <div className=' pt-4 flex flex-col gap-3'>
-                <WorkButton text='Video Games' color='border-red-500 text-red-100 hover:bg-red-500' />
-                <WorkButton text='Apps and Tools' color='border-emerald-500 text-emerald-100 hover:bg-emerald-500' />
-                <WorkButton text='Music' color='border-indigo-500 text-indigo-100 hover:bg-indigo-500' />
-                <WorkButton text='Illustrations' color='border-fuchsia-500 text-fuchsia-100 hover:bg-fuchsia-500' />
+            <WorkButton text='Video Games' color='border-red-500 text-red-100 hover:bg-red-500'                 
+                oneIsHovered={hoverCount > 0} 
+                hoverIncrementCallback={hoverIncrementCallback}
+                hoverDecrementCallback={hoverDecrementCallback}
+                />
+            <WorkButton text='Apps and Tools' color='border-emerald-500 text-emerald-100 hover:bg-emerald-500'  
+                oneIsHovered={hoverCount > 0} 
+                hoverIncrementCallback={hoverIncrementCallback}
+                hoverDecrementCallback={hoverDecrementCallback}
+                />
+            <WorkButton text='Music' color='border-indigo-500 text-indigo-100 hover:bg-indigo-500'              
+                oneIsHovered={hoverCount > 0} 
+                hoverIncrementCallback={hoverIncrementCallback}
+                hoverDecrementCallback={hoverDecrementCallback}
+                />
+            <WorkButton text='Illustrations' color='border-fuchsia-500 text-fuchsia-100 hover:bg-fuchsia-500'   
+                oneIsHovered={hoverCount > 0} 
+                hoverIncrementCallback={hoverIncrementCallback}
+                hoverDecrementCallback={hoverDecrementCallback}
+                />
         </div>
     </div>
 }
 
-function WorkButton(props: { text: string, color: string }) {
+function WorkButton(props: { text: string, color: string, oneIsHovered: boolean, hoverIncrementCallback: () => void, hoverDecrementCallback: () => void }) {
 
     return <ScrollAnimation initialClass='opacity-none' animationClass='blur-reveal-0'>
         <button className={`${props.color} font-bold text-sm w-fit border-4 lg:border-8 
-    hover:text-slate-950
-     rounded-tl-[50px] rounded-br-[50px] rounded-tr-xl rounded-bl-xl 
-
-     hover:rounded-tl-3xl hover:rounded-br-3xl hover:rounded-tr-xl hover:rounded-bl-xl transition-all duration-100 ease-in-out
-     hover:font-extrabold hover:pr-20 pr-16 pulse
-    `}>
-            <div className={` ml-5 
-
-                                pt-0.5 pb-1
+    
+     rounded-tl-[50px] rounded-br-[50px] rounded-tr-xl rounded-bl-xl pr-16 
+    hover:rounded-tl-3xl hover:rounded-br-3xl hover:rounded-tr-xl hover:rounded-bl-xl hover:text-slate-950 hover:font-extrabold
+     transition-all duration-100 ease-in-out
+     ${props.oneIsHovered ? 'work-button-others-hovered': 'work-button-idle'}
+     work-button-hovered
+    `}
+            onMouseEnter={props.hoverIncrementCallback}
+            onMouseLeave={props.hoverDecrementCallback}
+    >
+            <div className={` ml-5 pt-0.5 pb-1
                         text-sm 
                         sm:text-2xl
                         `}>
