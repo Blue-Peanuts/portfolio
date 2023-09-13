756c7b63b0e00052c84bc8421d8ab44b80111166
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-09-14 02:58:14 +0700

    good enough bg

--- src/Background/Background.tsx
+++ src/Background/Background.tsx
@@ -46,46 +46,49 @@ function Background() {
 
     // heavy refactor needed
 
+    const sections = 10;
+    const gridWidth = windowsWidth / 3;
+
     return <>
-        <div className=' h-screen w-screen absolute -z-10'>
+        { windowsWidth > 768 &&
+        <div className=' h-screen w-screen fixed -z-10'>
             <div style={
                 {
-                    top: roundToNearest(mousePos.y + scrollPosition - windowsWidth / 4, windowsWidth / 12),
-                    left: roundToNearest(mousePos.x - windowsWidth / 4, windowsWidth / 12),
+                    top: roundToNearest(mousePos.y + scrollPosition - gridWidth / 2, gridWidth / sections),
+                    left: roundToNearest(mousePos.x - gridWidth / 2, gridWidth / sections),
                     position: 'absolute',
-                    height: '50vw'
+                    height: `${gridWidth}px`,
+                    gridTemplateRows: `repeat(${sections}, minmax(0, 1fr))`,
+                    gridTemplateColumns: `repeat(${sections}, minmax(0, 1fr))`,
                 }
-            } className='aspect-square grid grid-cols-6 grid-rows-6'>
-                {Array.from(Array(36).keys()).map((i) => {
+            } className='aspect-square grid grid-cols-8 grid-rows-2'>
+                {Array.from(Array(sections ** 2).keys()).map((i) => {
 
-                    const x = i % 6;
-                    const y = Math.floor(i / 6);
-                    const gridPosX = roundToNearest(mousePos.x - windowsWidth / 4, windowsWidth / 12);
-                    const gridPosY = roundToNearest(mousePos.y + scrollPosition - windowsWidth / 4, windowsWidth / 12);
-                    const posX = gridPosX + (x + 0.5) * windowsWidth / 12;
-                    const posY = gridPosY + (y + 0.5) * windowsWidth / 12;
+                    const x = i % sections;
+                    const y = Math.floor(i / sections);
+                    const gridPosX = roundToNearest(mousePos.x - gridWidth / 2, gridWidth / sections);
+                    const gridPosY = roundToNearest(mousePos.y + scrollPosition - gridWidth / 2, gridWidth / sections);
+                    const posX = gridPosX + (x + 0.5) * gridWidth / sections;
+                    const posY = gridPosY + (y + 0.5) * gridWidth / sections;
                     const distX = Math.abs(posX - mousePos.x);
                     const distY = Math.abs(posY - mousePos.y - scrollPosition);
                     const dist = Math.sqrt(distX * distX + distY * distY);
                     return <div className=' flex justify-center items-center'> <div key={i} style={{
-                        height: `${
-                            Math.max(0, 100 - (dist*dist / windowsWidth * 2))
-                        }%`,
+                        height: `${Math.max(0, 120 - (dist * dist / gridWidth) * 2)
+                            }%`,
                     }} className='bg-slate-900 aspect-square text-xs rounded-full'>
-                    
+
                     </div>
                     </div>
                 }
                 )}
             </div>
         </div>
-        <div className=' h-screen w-screen fixed -z-10'>
-            <div className=' h-100 w-10 bg-white'>{mousePos.x} {mousePos.y + scrollPosition} {windowsWidth} {scrollPosition}</div>
-        </div>
+        }
     </>
 }
 function roundToNearest(value, interval) {
-  return Math.round(value/interval) * interval;
+    return Math.round(value / interval) * interval;
 }
 
 export default Background;
