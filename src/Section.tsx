f87d86a8b5c778c5ea5aa5eceb350c491284212c
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-09-14 17:27:24 +0700

    dynamic button

--- src/Section.tsx
+++ src/Section.tsx
@@ -28,32 +28,41 @@ function Section() {
                         
                         pt-12 pb-12
                         flex flex-row flex-wrap justify-start
-                        pl-[10vw]
+                        pl-0
+                        sm:pl-[10vw]
                         xl:pl-0
+
+                       rounded-xl
 
                         
                         '>
 
-                            <GameCard name='Gold Gobblers' img='https://img.itch.zone/aW1nLzEyODM2MzQ1LnBuZw==/180x143%23c/AZ1sD8.png' animateOrder={0}
+                        <GameCard name='Gold Gobblers' img='https://img.itch.zone/aW1nLzEyODM2MzQ1LnBuZw==/180x143%23c/AZ1sD8.png' animateOrder={0}
                             lines={['Real-time party-based parrying game.', 'Ranked #528 on The GMTK Jam 2023.', 'Worked on design, programming, music, sound effectts, particle effects.']} />
-                            <GameCard name='Gold Gobblers' img='https://img.itch.zone/aW1nLzEyODM2MzQ1LnBuZw==/180x143%23c/AZ1sD8.png' animateOrder={1}
+                        <GameCard name='Gold Gobblers' img='https://img.itch.zone/aW1nLzEyODM2MzQ1LnBuZw==/180x143%23c/AZ1sD8.png' animateOrder={1}
                             lines={['Real-time party-based parrying game.', 'The GMTK Jam 2023 submission.', 'Collaborated with an artist.']} />
-                            <GameCard name='Gold Gobblers' img='https://img.itch.zone/aW1nLzEyODM2MzQ1LnBuZw==/180x143%23c/AZ1sD8.png' animateOrder={1}
+                        <GameCard name='Gold Gobblers' img='https://img.itch.zone/aW1nLzEyODM2MzQ1LnBuZw==/180x143%23c/AZ1sD8.png' animateOrder={1}
                             lines={['Real-time party-based parrying game.', 'The GMTK Jam 2023 submission.', 'Collaborated with an artist.']} />
                     </div>
-                </div></div></div>
+                </div>
+            </div>
+        </div>
     </div>
 }
 
 
-function GameCard(props: {name: string, img: string, lines: string[], animateOrder: number})
-{
+function GameCard(props: { name: string, img: string, lines: string[], animateOrder: number }) {
     return <ScrollAnimation initialClass='opacity-none' animationClass={`blur-reveal-${props.animateOrder}`} className={`
         flex flex-col sm:flex-row gap-8 justify-center items-center w-fit lg:pl-10 pb-5 
         
     `}>
+        <div className='flex flex-col justify-center items-center'>
+            <img src={props.img} className=' max-h-48 rounded-xl' />
 
-        <img src={props.img} className=' max-h-48 rounded-xl' />
+            <div className='w-full'>
+                <button className='w-full block sm:hidden hover:bg-red-500 rounded-lg mt-2 font-bold border-4 border-red-500 hover:text-slate-950 text-red-500'> Play on Itch.io </button>
+            </div>
+        </div>
         <div className=' ml-0 w-[80vw] sm:w-64 sm:ml-6 flex flex-col justify-between items-center sm:items-start'>
             <span className=' text-white self-stretch'>
                 <p className=' text-xl font-bold pb-2'>
@@ -70,7 +79,7 @@ function GameCard(props: {name: string, img: string, lines: string[], animateOrd
                     }
                 </ul>
                 <div>
-                    <button className=' hover:bg-red-500 rounded-lg pl-2 pr-2 mt-2 font-bold border-4 border-red-500 hover:text-slate-950 text-red-500 ml-4 sm:ml-0'> Play on Itch.io </button>
+                    <button className=' hidden sm:block hover:bg-red-500 rounded-lg pl-2 pr-2 mt-2 font-bold border-4 border-red-500 hover:text-slate-950 text-red-500 ml-4 sm:ml-0'> Play on Itch.io </button>
                 </div>
             </span>
         </div>
